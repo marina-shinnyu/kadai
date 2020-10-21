5460f2ae242a905e27bceddf9d58704c24a8ab82
marina-shinnyu <mshinnyu30@gmail.com>
2020-10-21 02:19:06 +0000

    kadai-02/index.js

---
+++ kadai-02/index.js
@@ -0,0 +1,10 @@
+const firstname = 'Marina'
+const familyname = 'Shinnyu'
+console.log(firstname + familyname)
+console.log(1+2+3+4+5+6+7+8+9+10)
+
+const dog = false
+const cat = true
+console.log(dog || cat)
+console.log(dog && cat)
+console.log(!dog && !cat)
