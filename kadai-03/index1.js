e0f7733b158b90018e6cf2795a747c6a03964463
marina-shinnyu <mshinnyu30@gmail.com>
2020-10-28 04:58:03 +0000

    index2.js

--- kadai-03/index1.js
+++ kadai-03/index1.js
@@ -1,21 +1,10 @@
-score = 80
-
-
-if (score >= 80) {
-    console.log('優')
-} else if (score >= 70) {
-    console.log('良')
-} else if (score >= 60) {
-    console.log('可')
-} else {
-    console.log('不可')
-}
-
 const day = parseInt(process.argv[2])
 if (day >= 20) {
     console.log('あなたは慎重な性格です')
 } else if (day >= 10) {
     console.log('あなたは軽率な性格です')
-} else {
+} else if (day = 31) {
+    console.log('あなたは剽軽な性格です')
+} else
     console.log('あなたは普通の性格です')
 }
---
+++ kadai-03/index2.js
@@ -0,0 +1,8 @@
+const max = parseInt(process.argv[2])
+for(let n = 1; n <= max; n = n + 1){
+if(n% 3===0){
+console.log('油揚げをじっくり見る男性')
+}else{
+console.log(n)
+} 
+} 
